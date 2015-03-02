60aa594d990697c782de52c773c464e16d4617e8
Clavus <joerivdvelden@gmail.com>
2015-03-03 00:23:15 +0100

    Documented math util. Refactored a bit.

--- engine/extend/math.lua
+++ engine/extend/math.lua
@@ -4,57 +4,87 @@
 
 -- cache functions
 local math = math
-local sqrt, pi, random, floor, abs = math.sqrt, math.pi, math.random, math.floor, math.abs
+local sqrt, pi, random, floor, abs, cos = math.sqrt, math.pi, math.random, math.floor, math.abs, math.cos
 
-function math.distance( x1, y1, x2, y2 )
-	local xd = x2-x1
-	local yd = y2-y1
-	return sqrt( xd*xd + yd*yd )
-end
-
+--- Converts degrees to radians.
+-- @number deg angle in degrees
+-- @treturn number angle in radians
 function math.deg2rad( deg )
 	return deg / 180 * pi
 end
 
+--- Converts radians to degrees.
+-- @number rad angle in radians
+-- @treturn number angle in degrees
 function math.rad2deg( rad )
 	return rad / pi * 180
 end
 
+--- Clamps a number between a minimum and maximum.
+-- @number num input number
+-- @number low minimum
+-- @number high maximum
+-- @treturn number clamped number
 function math.clamp( num, low, high )
 	return num < low and low or (num > high and high or num)
 end
+local clamp = math.clamp
 
+--- Returns a random (rational) number between the given minimum and maximum.
+-- @number low minimum
+-- @number high maximum
+-- @treturn number random number
 function math.randomRange( low, high )
 	return low + (random() * (high-low))
 end
 
+--- Returns the sign of the number. Meaning it returns -1 if the number < 0, otherwise returns 1.
+-- @number x input number
+-- @treturn number sign number (-1 or 1)
 function math.sign( x )
 	return x < 0 and -1 or 1
 end
 
+--- Rounds a number to the nearest given amount of decimals.
+-- @number i input number
+-- @number[opt=0] decimals amount of decimals
+-- @treturn number rounded number
 function math.round( i, decimals )
 	local mul = 10^(decimals or 0)
   return floor(i * mul + 0.5) / mul
 end
 
-function math.approach( cur, target, inc ) -- sets <inc> step from <cur> to <target>
+--- Approach target number from current number with a given increment.
+-- @number cur input number
+-- @number target target number
+-- @number inc increment
+-- @treturn number result
+function math.approach( cur, target, inc )
 	inc = abs( inc )
 	if (cur < target) then
-		return math.clamp( cur + inc, cur, target )
+		return clamp( cur + inc, cur, target )
 	elseif (cur > target) then
-		return math.clamp( cur - inc, target, cur )
+		return clamp( cur - inc, target, cur )
 	end
 	return target
 end
 
+--- Lerp between two numbers with a given fraction.
+-- @number a first number
+-- @number b second number
+-- @number frac fraction
+-- @treturn number result
 function math.lerp(a, b, frac) -- <frac> is in the range of 0 to 1
-	assert(frac <= 1 and frac >= 0, "Lerp fraction has to be between 0 and 1")
-	return a + (b - a) * frac
+	return a + (b - a) * clamp(frac, 0, 1)
 end
 
+--- Lerp using cosine interprolation between two numbers with a given fraction.
+-- @number a first number
+-- @number b second number
+-- @number frac fraction
+-- @treturn number result
 function math.smooth(a, b, frac) -- same as math.lerp but with cosine interpolation
-	local m = (1 - math_cos(lume.clamp(frac, 0, 1) * math_pi)) / 2
+	local m = (1 - cos(clamp(frac, 0, 1) * pi)) / 2
 	return a + (b - a) * m
 end
-
 
--- engine/util/angle.lua
+++ engine/util/angle.lua
@@ -27,26 +27,27 @@ function angle.normalize( r )
 	end
 	return r
 end
+local normalize = angle.normalize
 
 --- Rotates angle by given radians, normalizes angle after (between -pi and pi).
 -- @number r1 angle (radians)
 -- @number r2 angle (radians) to rotate by
 -- @treturn number new angle (radians)
 function angle.rotate( r1, r2 )
-	return angle.normalize( r1 + r2 )
+	return normalize( r1 + r2 )
 end
 
---- Rotates angle towards another angle by the given step.
+--- Rotates angle towards another angle with a given increment.
 -- @number r1 angle (radians)
 -- @number r2 angle (radians) to rotate towards
--- @number rstep angle (radians) to step from r1 to r2.
+-- @number incr angle (radians) to increment from r1 to r2.
 -- @treturn number new angle (radians)
-function angle.rotateTo( r1, r2, rstep )
-	local nr =  angle.normalize( r2 - r1 )
+function angle.rotateTo( r1, r2, incr )
+	local nr =  normalize( r2 - r1 )
 	if (nr < 0) then
-		return r1 - min(rstep, -nr)
+		return r1 - min(incr, -nr)
 	else
-		return r1 + min(rstep, nr)
+		return r1 + min(incr, nr)
 	end
 end
 
@@ -56,12 +57,11 @@ end
 -- @number frac fraction (between 0 and 1) to go from r1 to r2.
 -- @treturn number new angle (radians)
 function angle.lerp( r1, r2, frac )
-	local nr =  angle.normalize( r2 - r1 )
-	frac = math.clamp(frac, 0, 1)
+	local nr = normalize( r2 - r1 )
 	if (nr < 0) then
-		return r1 - nr * frac
+		return r1 - nr * clamp(frac, 0, 1)
 	else
-		return r1 + nr * frac
+		return r1 + nr * clamp(frac, 0, 1)
 	end
 end
 
