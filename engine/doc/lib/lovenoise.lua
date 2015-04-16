aa16d5ab670a4431e11a0c31abb01b9ea259cd3e
Joeri van der Velden <joerivdvelden@gmail.com>
2015-04-16 15:39:17 +0200

    Usage example of noise lib

--- engine/doc/lib/lovenoise.lua
+++ engine/doc/lib/lovenoise.lua
@@ -2,5 +2,23 @@
 -- Noise library.
 -- https://github.com/icrawler/LoveNoise
 -- @lib noise
+-- @usage -- Reference lovenoise.modules (optional)
+-- local modules = lovenoise.modules
+-- 
+-- -- Creating a new Simplex module
+-- local simplexMod = modules.Simplex:new()
+-- 
+-- -- Setting its properties (see doc)
+-- simplexMod:setFrequency(0.01)
+-- simplexMod:setSeed(math.random()*600)
+-- 
+-- -- Creating a new RidgedMulti module with 4 octaves (with condensed property setters)
+-- local ridgedMod = modules.RidgedMulti:new(4):setFrequency(0.01):setSeed(math.random()*600)
+-- 
+-- -- Creating a new Min module with the previous modules as its sources
+-- local minMod = modules.Min:new(ridgedMod, simplexMod)
+-- 
+-- -- Getting a value at location [5, 6]
+-- local val = modules.Min:getValue(5, 6)
 
 -- THIS IS A FAKE IMPLEMENTATION JUST TO ASSIST DOCUMENTATION GENERATION. DO NOT INCLUDE IN THE FRAMEWORK.
